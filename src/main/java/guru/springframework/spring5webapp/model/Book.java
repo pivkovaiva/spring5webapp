13a8dbea7ec2a3751f13d50a5cbe2ddd42d545b3
ipivkova <iva.pivkova@endava.com>
2019-06-10 17:07:58 +0200

    Author and Book entities added

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
